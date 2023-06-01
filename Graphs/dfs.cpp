70b0e63b8ce0ce05d6fff83ad73b166bd5437f51
Adarsh <adarshriit2017@gmail.com>
2023-06-02 04:25:06 +0530

    02062023-1

---
+++ Graphs/dfs.cpp
@@ -0,0 +1,31 @@
+class Solution 
+{
+
+    vector<int> bfsOfGraph(int V, vector<int> adj[])
+    {
+        int vis[V] = {0};
+        vis[0] = 1;
+
+        queue<int> q;
+        q.push_back(0);
+
+        vector<int> ans;
+
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+            for(auto i : adj[node])
+            {
+                if(!vis[i])
+                {
+                    vis[i] = 1;
+                    q.push(i);
+                }
+            }
+        } 
+        return bfs;
+    }
+
+};
